504d5a84d17142b634055d5b9f731e8d8ed97c98
Guenael Roger <guenael.mail@gmail.com>
2026-03-09 18:10:17 +0100

    added symmetry regex to obersvation manager config

--- src/mjlab/managers/manager_term_config.py
+++ src/mjlab/managers/manager_term_config.py
@@ -119,6 +119,8 @@ class ObservationTermCfg(ManagerTermBaseCfg):
   When True and concatenate_terms=True, uses term-major ordering:
   [A_t0, A_t1, ..., A_tH-1, B_t0, B_t1, ..., B_tH-1, ...]
   See docs/api/observation_history_delay.md for details on ordering."""
+  symmetry_regex: str = r"(?i).*_(roll|yaw)$"
+  """  """  # TODO
 
 
 @dataclass
