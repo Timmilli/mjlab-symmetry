fa5bc788b2e6e0b6ef212ec7f803d0e4a38a2834
Guenael Roger <guenael.mail@gmail.com>
2026-03-16 11:33:21 +0100

    changed foot_height and air_time to class

--- src/mjlab/tasks/velocity/mdp/observations.py
+++ src/mjlab/tasks/velocity/mdp/observations.py
@@ -18,19 +18,35 @@ if TYPE_CHECKING:
 _DEFAULT_ASSET_CFG = SceneEntityCfg("robot")
 
 
-def foot_height(
-    env: ManagerBasedRlEnv, asset_cfg: SceneEntityCfg = _DEFAULT_ASSET_CFG
-) -> torch.Tensor:
-    asset: Entity = env.scene[asset_cfg.name]
-    return asset.data.site_pos_w[:, asset_cfg.site_ids, 2]  # (num_envs, num_sites)
+class foot_height:
+    def __init__(self, env: ManagerBasedRlEnv, cfg: ObservationTermCfg):
+        self.env = env
+        self.cfg = cfg
+
+    def __call__(
+        self, env: ManagerBasedRlEnv, asset_cfg: SceneEntityCfg = _DEFAULT_ASSET_CFG
+    ) -> torch.Tensor:
+        asset: Entity = env.scene[asset_cfg.name]
+        return asset.data.site_pos_w[:, asset_cfg.site_ids, 2]  # (num_envs, num_sites)
+
+    def apply_symmetry(self, obs: torch.Tensor):
+        obs[:, [0, 1]] = obs[:, [1, 0]]
 
 
-def foot_air_time(env: ManagerBasedRlEnv, sensor_name: str) -> torch.Tensor:
-    sensor: ContactSensor = env.scene[sensor_name]
-    sensor_data = sensor.data
-    current_air_time = sensor_data.current_air_time
-    assert current_air_time is not None
-    return current_air_time
+class foot_air_time:
+    def __init__(self, env: ManagerBasedRlEnv, cfg: ObservationTermCfg):
+        self.env = env
+        self.cfg = cfg
+
+    def __call__(self, env: ManagerBasedRlEnv, sensor_name: str) -> torch.Tensor:
+        sensor: ContactSensor = env.scene[sensor_name]
+        sensor_data = sensor.data
+        current_air_time = sensor_data.current_air_time
+        assert current_air_time is not None
+        return current_air_time
+
+    def apply_symmetry(self, obs: torch.Tensor):
+        obs[:, [0, 1]] = obs[:, [1, 0]]
 
 
 class foot_contact:
